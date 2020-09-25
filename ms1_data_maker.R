096542d1fdf707fb284ad72fba2ab3f158e43486
wkumler <wkumler@uw.edu>
2020-09-25 16:21:36 -0700

    Adding more notes and M+H delta

--- ms1_data_maker.R
+++ ms1_data_maker.R
@@ -35,10 +35,19 @@ grabSingleFileData <- function(filename){
 # rather than my Falkor ones. You'll probably also need to change the "pattern"
 # to .mzXML since I'm being weird and extracting my files separately.
 # Honestly, you could even paste the file names in here manually, as long as it's
-# a character vector of full file names the rest of the code should run
+# a character vector of full file names the rest of the code should run.
+# Mine looks like:
+# c("G:\\My Drive\\FalkorFactor\\mzMLs\\pos\\190715_Poo_TruePooFK180310_Full1.mzML",
+#   "G:\\My Drive\\FalkorFactor\\mzMLs\\pos\\190715_Poo_TruePooFK180310_Full2.mzML",
+#   "G:\\My Drive\\FalkorFactor\\mzMLs\\pos\\190715_Poo_TruePooFK180310_Full3.mzML",
+#   "G:\\My Drive\\FalkorFactor\\mzMLs\\pos\\190715_Poo_TruePooFK180310_Half1.mzML",
+#   "G:\\My Drive\\FalkorFactor\\mzMLs\\pos\\190715_Poo_TruePooFK180310_Half2.mzML",
+#   "G:\\My Drive\\FalkorFactor\\mzMLs\\pos\\190715_Poo_TruePooFK180310_Half3.mzML"
+# ) -> ms_file_paths
 ms_file_paths <- dir(r"(G:\My Drive\FalkorFactor\mzMLs\pos)",
                      pattern = "\\.mzML",
                      full.names = TRUE) %>%
+  grep(pattern = "Poo", x = ., value = TRUE) %>%
   normalizePath()
 # Check that all files can be found
 if(!all(file.exists(ms_file_paths))){
@@ -57,6 +66,8 @@ pep_masses <- c(mono_peptides$value, di_peptides$value)
 
 
 # Grab data points within ppm of the peptide mass across all RTs
+# This is the long step - each file needs to be opened up and organized,
+# but the progress bar should give you an estimate of time required.
 ms1_data <- pblapply(ms_file_paths, function(file_path){
   # Open up the mzML as a data.frame with mz, rt, int columns
   raw_EIC <- grabSingleFileData(file_path)
@@ -66,8 +77,9 @@ ms1_data <- pblapply(ms_file_paths, function(file_path){
   
   # For each mono/di peptide, grab the parts of the mzML that have an m/z 
   # close to (within `ppm`) the peptide mass
+  # Assume an H+ adduct and add 1.007276
   lapply(unique(pep_masses), function(mz_i){
-    raw_EIC_dt[mz%between%pmppm(as.numeric(mz_i), ppm=5)]
+    raw_EIC_dt[mz%between%pmppm(as.numeric(mz_i), ppm=5)+1.007276]
   }) %>% do.call(what = rbind) %>%
     cbind(fileid=basename(file_path))
 }) %>% do.call(what = rbind)
